543e9aed6ebd8bd0cb3fc79dfebe0cb520e03722
DorinSuciu <git@github.com:mascalucian/fe-finalproject.git>
2021-09-07 19:45:42 +0300

    created test for PUT

--- AspNetSandbox.Tests/BooksServiceTests.cs
+++ AspNetSandbox.Tests/BooksServiceTests.cs
@@ -11,7 +11,7 @@ namespace AspNetSandbox.Tests
     {
 
         [Fact]
-        public void BooksServiceTest()
+        public void BooksServicePostTest()
         {
             //Asume
             var booksService = new BooksService();
@@ -34,6 +34,33 @@ namespace AspNetSandbox.Tests
 
             // Assert
             Assert.Equal("Test Book Nr1", booksService.Get(3).Title);
+        }
+
+        [Fact]
+        public void BooksServicePutTest()
+        {
+            //Asume
+            var booksService = new BooksService();
+
+            // Act
+            booksService.Put(1, new Book
+            {
+                Id = 1,
+                Title = "Test Book Nr1",
+                Author = "Tester 1 ",
+                Language = "test 1"
+            }) ;
+            //booksService.Delete(2);
+            //booksService.Post(new Book
+            //{
+            //    Title = "Test Book Nr2",
+            //    Author = "Tester 2",
+            //    Language = "test 2"
+            //});
+
+
+            // Assert
+            Assert.Equal("Test Book Nr1", booksService.Get(1).Title);
         }
     }
 }
