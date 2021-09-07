225fab7431706deef2ddeef1948c58de2f5fc318
DorinSuciu <git@github.com:mascalucian/fe-finalproject.git>
2021-09-07 11:59:54 +0300

    created first Test

---
+++ AspNetSandbox.Tests/BooksServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspNetSandbox.Tests
+{
+    public class BooksServiceTests
+    {
+
+        [Fact]
+        public void ConvertResponseToWeatherForecastTest()
+        {
+            //Asume
+            var booksService = new BooksService();
+
+            // Act
+            booksService.Post(new Book
+            {
+                Title = "Test Book Nr1",
+                Author = "Tester 1 ",
+                Language = "test 1"
+            });
+            booksService.Delete(2);
+            booksService.Post(new Book
+            {
+                Title = "Test Book Nr2",
+                Author = "Tester 2",
+                Language = "test 2"
+            });
+
+
+            // Assert
+            Assert.Equal("Test Book Nr1", booksService.Get(3).Title);
+        }
+    }
+}
