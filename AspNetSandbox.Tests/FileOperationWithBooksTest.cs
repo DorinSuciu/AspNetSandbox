b27a842d3788ca6a8b1a58584c477c5cf6fb3ab9
DorinSuciu <git@github.com:mascalucian/fe-finalproject.git>
2021-09-22 16:11:58 +0300

    created CreateFileTest

--- AspNetSandbox.Tests/FileOperationWithBooksTest.cs
+++ AspNetSandbox.Tests/FileOperationWithBooksTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,29 @@ namespace AspNetSandbox.Tests
             {
                 Console.WriteLine(file.Name);
             }
+        }
+
+        [Fact]
+        public void CreateFileTest()
+        {
+            File.WriteAllText("newSettings.json", @"{
+  ""ConnectionStrings"": {
+    ""LocalPostgresConnection"": ""Server=127.0.0.1;Port=5432;Database=aspNetSandbox-dorin;User Id=postgres;Password=0eb55f5004111d434b7967c45ad9b780;"",
+    ""SqlConnection"": ""Server=(localdb)\\mssqllocaldb;Database=aspnet-AspNetSandbox-EE9A05D4-6579-4FB7-86C4-043D2F4A39E8;Trusted_Connection=True;MultipleActiveResultSets=true"",
+    ""Heroku"": ""Server=ec2-54-155-61-133.eu-west-1.compute.amazonaws.com; Port=5432; Database=d9kf2d06613b69; User Id=pwfaygvwgqjujq; Password=764b5d558a31c711a203ef631722167e2832998c24d748ea4dcc29ef9497d09d; SslMode=Require; Trust Server Certificate=true"",
+    ""HerokuRaw"": ""postgres://pwfaygvwgqjujq:764b5d558a31c711a203ef631722167e2832998c24d748ea4dcc29ef9497d09d@ec2-54-155-61-133.eu-west-1.compute.amazonaws.com:5432/d9kf2d06613b69"",
+    ""DefaultConnection"": ""Port=5432; Database=SandboxDb; Host=localhost; User Id=postgres;Password=0eb55f5004111d434b7967c45ad9b780; Trust Server Certificate=true""
+  },
+  ""Logging"": {
+    ""LogLevel"": {
+      ""Default"": ""Information"",
+      ""Microsoft"": ""Warning"",
+      ""Microsoft.Hosting.Lifetime"": ""Information""
+    }
+  },
+  ""AllowedHosts"": ""*""
+}
+");
         }
     }
 }
