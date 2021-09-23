a660441ad73ab58115ae495f9712bef8877cfb0d
DorinSuciu <git@github.com:mascalucian/fe-finalproject.git>
2021-09-23 17:11:23 +0300

    propagated command line args to the configuration

--- AspNetSandbox/Program.cs
+++ AspNetSandbox/Program.cs
@@ -16,6 +16,12 @@ namespace AspNetSandbox
         {
             [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
             public bool Verbose { get; set; }
+
+            [Option('c', "connection", Required = false, HelpText = "Choose connection string --c [connectionStringValue].")]
+            public bool ConnectionString { get; set; }
+
+            [Value(0, MetaName = "connectionValue", Required = false, HelpText = "Place value for the connection string.")]
+            public string ConnectionStringValue { get; set; }
         }
 
         public static int Main(string[] args)
@@ -32,6 +38,11 @@ namespace AspNetSandbox
                       {
                           Console.WriteLine($"Current Arguments: -v {o.Verbose}");
                           Console.WriteLine("Quick Start Example!");
+                      }
+
+                      if (o.ConnectionString && o.ConnectionStringValue == null)
+                      {
+                          Console.WriteLine("Insert value for connection string!");
                       }
                   });
             if (args.Length > 0)
